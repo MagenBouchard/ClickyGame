0bbdf69a94571a0da69beef16d952f50c92b9793
Michael Pascuzzi <gatorpazz@Michaels-MacBook-Pro.local>
2020-02-22 10:02:45 -0500

    Updates

---
+++ precache-manifest.a4b0bb739706ff8418e2e3f5d9ed80f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8addf1f624adb8158aeb29a84c0f59eb",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "d7aab59e7837a509572e",
+    "url": "/ClickyGame/static/css/main.bbc18b64.chunk.css"
+  },
+  {
+    "revision": "b7f2699c3aee66780254",
+    "url": "/ClickyGame/static/js/2.3f6b2f78.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickyGame/static/js/2.3f6b2f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7aab59e7837a509572e",
+    "url": "/ClickyGame/static/js/main.5d7bd5f1.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  }
+]);
